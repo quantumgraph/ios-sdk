18d2c99626fec3656950e347b8ee882747873be7
Rophy Tsai <rophy.tsai@appier.com>
2020-06-01 15:24:06 +0800

    set LANG and add allow-warnings

--- publish.sh
+++ publish.sh
@@ -2,11 +2,13 @@
 
 set -e
 
+export LANG=en_US.UTF-8
+
 VERSION=$(cat version.json | jq -r .version)
 GIT_TAG=v$VERSION
 git tag -a -m "tagging $GIT_TAG" $GIT_TAG
 git push -u origin $GIT_TAG
 pod spec lint quantumgraph.podspec
-pod trunk push quantumgraph.podspec
+pod trunk push quantumgraph.podspec --allow-warnings
 pod spec lint Appier.podspec
-pod trunk push Appier.podspec
+pod trunk push Appier.podspec --allow-warnings
