bda67f5f2edbd552fd24c0b663f399584b07b900
rex.chen <rex.chen@appier.com>
2020-09-29 10:44:44 +0800

    Make sure to clean cache before podspec validation

--- publish.sh
+++ publish.sh
@@ -8,6 +8,7 @@ VERSION=$(cat version.json | jq -r .version)
 GIT_TAG=v$VERSION
 git tag -a -m "tagging $GIT_TAG" $GIT_TAG
 git push -u origin $GIT_TAG
+pod cache clean --all # will clean all pods
 pod spec lint quantumgraph.podspec
 pod trunk push quantumgraph.podspec --allow-warnings
 pod spec lint Appier.podspec
